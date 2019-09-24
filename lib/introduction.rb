cf1fabe395c0ae409c529924225907c5a0bd746f
Kimberly Huynh <huynh.h.kimberly@gmail.com>
2019-09-24 01:14:46 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,16 +4,16 @@
 # in order to complete the rest of this course and be ready for the next course!
 
 #introduction(name)
-puts "Hi, my name is #{name}."
-end
+#puts "Hi, my name is #{name}."
+#end
 
 #introduction_with_language(name, language)
-puts "Hi, my name is #{name} and I am learning to program in #{language}."
-end
+#puts "Hi, my name is #{name} and I am learning to program in #{language}."
+#end
 
 #introduction_with_language_optional(name, language = "Ruby")
-puts "Hi, my name is #{name} and I am learning to program in #{language}."
-end
+#puts "Hi, my name is #{name} and I am learning to program in #{language}."
+#end
 
 
 
